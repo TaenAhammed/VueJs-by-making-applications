b2642b9a4cbdec4a99147ba58a65170ac8fa0597
taen <taenahammed7@gmail.com>
2018-09-23 22:29:24 +0600

    automatically computed value and v-model for input field

--- VueJs-by-making-applications/app.js
+++ VueJs-by-making-applications/app.js
@@ -2,11 +2,19 @@ new Vue({
     el: "#app",
     data: {
         items: [
-            { name: "Rice", price: "10.40" },
-            { name: "Wheat", price: "10.60" },
-            { name: "Rice", price: "20.00" }
+            { name: "Rice", price: 10.4 },
+            { name: "Wheat", price: 10.6 },
+            { name: "Rice", price: 20.0 }
         ],
         newItem: ""
+    },
+    computed: {
+        totalPrice() {
+            let total = this.items.reduce((sum, item) => {
+                return (sum += parseFloat(item.price));
+            }, 0);
+            return total;
+        }
     },
     methods: {
         addItem() {
