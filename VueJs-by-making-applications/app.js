5c9f1558d794d25dbc84b57790f1f3556fca231e
taen <taenahammed7@gmail.com>
2018-09-23 21:48:42 +0600

    v-for -- rendering list

---
+++ VueJs-by-making-applications/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        items: [
+            { name: "Rice", price: "10.40" },
+            { name: "Wheat", price: "10.60" },
+            { name: "Rice", price: "20.00" }
+        ]
+    }
+});
