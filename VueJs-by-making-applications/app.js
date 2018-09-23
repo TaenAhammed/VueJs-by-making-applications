44838a6bb897043d315fea84fbbfb9f1577fcafa
taen <taenahammed7@gmail.com>
2018-09-23 21:57:02 +0600

    v-model -- two way binding @submit.prevent -- prevent default form

--- VueJs-by-making-applications/app.js
+++ VueJs-by-making-applications/app.js
@@ -5,6 +5,12 @@ new Vue({
             { name: "Rice", price: "10.40" },
             { name: "Wheat", price: "10.60" },
             { name: "Rice", price: "20.00" }
-        ]
+        ],
+        newItem: ""
+    },
+    methods: {
+        addItem() {
+            this.items.push({ name: this.newItem, price: 0.0 });
+        }
     }
 });
